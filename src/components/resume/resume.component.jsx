e69b4f529293d2f794a49382fda152758e05ade4
Alec Iberg <ibergalec@gmail.com>
2022-07-09 11:28:01 -0700

    resume embedded

--- src/components/resume/resume.component.jsx
+++ src/components/resume/resume.component.jsx
@@ -1,7 +1,24 @@
+import React, { useState } from 'react';
+
+import resume from './alecibergresume.pdf';
+
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 const Resume = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   return (
     <div>
-      <h2>This will be a resume</h2>
+      <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
     </div>
   );
 };
