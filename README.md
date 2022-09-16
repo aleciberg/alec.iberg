7438eb3655412b90db89186761f35b96f43e6a7a
Alec Iberg <ibergalec@gmail.com>
2022-09-16 10:58:54 -0700

    updated about me copy and deploy process

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ In Terminal: <br>
 `npm run deploy`
 
 GH Pages Settings need to be `master` and have the correct custom domain.
+
+Seems to take a bit- just be patient.
+Do I have to cycle between the gh-pages and master branch or am I just impatient?
+Will try again when I publish new paragraph.
--- src/components/about-me/about-me.component.jsx
+++ src/components/about-me/about-me.component.jsx
@@ -27,25 +27,28 @@ const AboutMe = () => {
           Hello, I am <span className={'outdoors'}>Alec Iberg</span> and I am a
           Full Stack Developer.
           <br></br>
-          <br></br>I was most recently employed at Olive, working on the Olive
-          Platform application. The stack included{' '}
-          <span className="target">TypeScript, React, NodeJS, and GoLang </span>
-          and I was able to work in these everyday. I always find great
-          satisfaction in seeing my work produce{' '}
-          <span className="target">meaningful </span> and
-          <span className="target"> measurable</span> results in the HealthCare
-          space, a space I am very passionate about improving for everyone.{' '}
+          <br></br>I currently work at Array, as a Full Stack Developer,
+          building FinTech tools. I currently work with{' '}
+          <span className="target">
+            JavaScript, TypeScript, Svelte, NodeJS, and GoLang{' '}
+          </span>
+          and very much enjoy this stack. I always find great satisfaction in
+          seeing my work produce <span className="target">meaningful </span> and
+          <span className="target"> measurable</span> results and I love being
+          able to provide value to the FinTech space and improve access to
+          credit for <span className="target">everyone.</span>{' '}
         </Typography>
         <Typography variant="h5" align="center" sx={{ mb: 1.5 }}>
           As for my personal life, I really enjoy the{' '}
           <span className="outdoors">outdoors</span>- you can usually find me
           camping, hiking, or backpacking on a weekend. I am fortunate enough to
           <span className="target"> live in Southern California</span> and be
-          surrounded by some of our nation's best National Parks. Joshua Tree is
-          probably my favorite. Additionally, I am a huge College Football fan
-          (GO BUCKS!), a big hockey fan, and I very much enjoy spending time
-          with my dog, Juniper. She is a 2 year old Border Collie with unlimited
-          energy.
+          surrounded by some of our nation's best National Parks.{' '}
+          <span className={'outdoors'}>Joshua Tree</span> is probably my
+          favorite. Additionally, I am a huge College Football fan (GO BUCKS!),
+          a big hockey fan, and I very much enjoy spending time with my dog,
+          <span className={'outdoors'}> Juniper.</span> She is a 2 year old
+          Border Collie with unlimited energy.
         </Typography>
       </CardContent>
     </Card>
