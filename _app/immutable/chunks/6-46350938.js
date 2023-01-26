17d2b17f3ea8b75dd3316a364e4c622ed9bb07c6
Alec Iberg <ibergalec@gmail.com>
2023-01-25 16:39:00 -0800

    Updates

---
+++ _app/immutable/chunks/6-46350938.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/Skills/_page.svelte-2e083743.js";export{t as component};
--- _app/immutable/chunks/6-9ca11795.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/Skills/_page.svelte-915de412.js";export{t as component};
